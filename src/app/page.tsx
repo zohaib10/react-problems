f4d3e6d83ecb70558c3f7cfc048b7ae7b16b596b
Zohaib Ahmad <zohaibahmad@Zohaibs-MacBook-Pro.local>
2025-05-31 00:16:34 -0700

    problem 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center h-dvh bg-gray-800">
       <Link
-        href="/lesson/1"
+        href="/problem/1"
         className="border p-4 rounded-sm bg-pink-600 border-pink-600"
       >
         Counter Component
